39496142e9902f379050f08822c79375fa667e8b
Henrique Barros <henrique1_barros@hotmail.com>
2023-09-27 16:37:20 -0300

    feat/inoa-9:  edit and exclude

--- stock_users/urls.py
+++ stock_users/urls.py
@@ -6,4 +6,6 @@ app_name = 'stock_users'
 
 urlpatterns = [
     path('<int:user_id>/', UserStocksView.as_view(), name='stock_users'),
+    path('delete/', UserStocksView.as_view(), name='delete'),
+    path('update/', UserStocksView.as_view(), name='update'),
 ]
--- stock_users/views.py
+++ stock_users/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from django.views import View
 from users.models import User
 from stock_users.models import StockUser
+from django.shortcuts import render
+from decimal import Decimal
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 # Create your views here.
 
@@ -14,3 +18,34 @@ class UserStocksView(View):
             return render(request, 'stock_users/index.html', {'user': user, 'stocks': stock_users})
         except User.DoesNotExist:
             return render(request, 'user_not_found.html')
+        
+    def post(self, request):
+            
+            updated_stock_user_id = request.POST.get('updated_stock_user_id')
+
+            if(updated_stock_user_id):
+                 
+                stock_user = StockUser.objects.get(pk=updated_stock_user_id)
+
+                selling_price = request.POST.get('selling_price')
+                buying_price = request.POST.get('buying_price')
+                is_notifying = request.POST.get('is_notifying')
+                update_period = request.POST.get('update_period')
+
+                selling_price = Decimal(selling_price.replace(".", "").replace(",", "."))
+                buying_price = Decimal(buying_price.replace(".", "").replace(",", "."))
+
+                stock_user.selling_price = selling_price
+                stock_user.buying_price = buying_price
+                stock_user.is_notifying = is_notifying
+                stock_user.update_period = update_period
+                stock_user.save()
+
+                return HttpResponseRedirect(reverse('stock_users:stock_users', args=[stock_user.user.id]))
+
+            else:
+                stock_user_id = request.POST.get('stock_user')
+                stock_user = StockUser.objects.get(pk=stock_user_id)
+                stock_user.delete()
+                
+                return HttpResponseRedirect(reverse('stock_users:stock_users', args=[stock_user.user.id]))
