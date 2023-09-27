29c1e858d610711ed70a2d15fcc166961cad7a20
Henrique Barros <henrique1_barros@hotmail.com>
2023-09-26 22:48:07 -0300

    feat/inoa-7: envio de emails

--- stock_users/management/commands/updateStocks.py
+++ stock_users/management/commands/updateStocks.py
@@ -2,6 +2,9 @@ from django.core.management.base import BaseCommand
 from stock_users.models import StockUser
 import requests
 from django.utils import timezone
+import smtplib
+from email.message import EmailMessage
+from jinja2 import Template
 
 class Command(BaseCommand):
     help = 'Script para atualizar ações do banco de dados.'
@@ -25,7 +28,6 @@ class Command(BaseCommand):
         stock_string = ",".join(stocks)
         api_url = f"https://brapi.dev/api/quote/{stock_string}"
         response = requests.get(api_url)
-
         data = None
 
         if response.status_code == 200:
@@ -49,11 +51,79 @@ class Command(BaseCommand):
             return False
         
         if(newPrice >= stock_user.selling_price):
-            Command.notify("sell")
+            Command.notifySell(stock_user)
         elif(newPrice <= stock_user.buying_price):
-            Command.notify("buy")
+            Command.notifyBuy(stock_user)
         
-    def notify(string):
-        return print(string)
+    def notifyBuy(stock_user):
+
+        EMAIL_ADDRESS = "inoateste@gmail.com"
+        EMAIL_PASSWORD = "ejqm unah bznr mxak"
+
+        html_template = '''
+            <div class="container text-center mt-5">
+                <div class="alert alert-success">
+                    Olá {{ username }}! A ação <strong>{{ stock_name }}</strong> está dentro do preço de compra ideal para você.
+                </div>
+                <p class="mt-3">R$ {{ stock_close }}</p>
+            </div>
+        '''
+        template = Template(html_template)
+        rendered_html = template.render(
+            stock_logo=stock_user.stock.logo,
+            stock_close=stock_user.close,
+            username=stock_user.user.username,
+            stock_name=stock_user.stock.name
+        )
+
+        msg = EmailMessage()
+        msg['Subject'] = "Notificação de monitoramento"
+        msg['From'] = EMAIL_ADDRESS
+        msg['To'] = stock_user.user.email
+        msg.set_content(rendered_html, subtype='html')
+
+        try:
+            with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+                smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+                smtp.send_message(msg)
+            print("Email sent successfully")
+        except Exception as e:
+            print("Error sending email:", str(e))
+
+    def notifySell(stock_user):
+
+        EMAIL_ADDRESS = "inoateste@gmail.com"
+        EMAIL_PASSWORD = "ejqm unah bznr mxak"
+
+        html_template = '''
+            <div class="container text-center mt-5">
+                <div class="alert alert-success">
+                    Olá {{ username }}! A ação <strong>{{ stock_name }}</strong> está dentro do preço de venda ideal para você.
+                </div>
+                <p class="mt-3">R$ {{ stock_close }}</p>
+            </div>
+        '''
+
+        template = Template(html_template)
+        rendered_html = template.render(
+            stock_logo=stock_user.stock.logo,
+            stock_close=stock_user.close,
+            username=stock_user.user.username,
+            stock_name=stock_user.stock.name
+        )
+
+        msg = EmailMessage()
+        msg['Subject'] = "Notificação de monitoramento"
+        msg['From'] = EMAIL_ADDRESS
+        msg['To'] = stock_user.user.email
+        msg.set_content(rendered_html, subtype='html')
+
+        try:
+            with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+                smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+                smtp.send_message(msg)
+            print("Email sent successfully")
+        except Exception as e:
+            print("Error sending email:", str(e))
 
         
